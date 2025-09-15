33148bc693a5253abfc45f64bc562e5747d527f9
Fiery-Mouwnies <166019254+Fiery-Mouwnies@users.noreply.github.com>
2025-09-15 23:20:21 +0200

    Add Scene component code

---
+++ src/Scene.jsx
@@ -0,0 +1,17 @@
+import React, { useRef, useEffect } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { Environment, PerspectiveCamera } from '@react-three/drei';
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const Scene = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Scene
