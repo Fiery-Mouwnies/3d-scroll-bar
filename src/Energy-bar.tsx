b616cbbbf03aff26fa34773a257814a5ea7f5217
Fiery-Mouwnies <166019254+Fiery-Mouwnies@users.noreply.github.com>
2025-09-16 13:14:52 +0200

    Updated Energy-bar.tsx

--- src/Energy-bar.tsx
+++ src/Energy-bar.tsx
@@ -1,26 +1,54 @@
-import React, { forwardRef } from 'react';
-import { useGLTF } from '@react-three/drei';
-import { GroupProps } from '@react-three/fiber';
 import * as THREE from 'three';
+import React, { useMemo, useContext, createContext, forwardRef } from 'react';
+import { useGLTF, Merged } from '@react-three/drei';
+import { GLTF } from 'three-stdlib';
+import { GroupProps } from '@react-three/fiber';
 
-// Note: This component assumes you have run the model through the online converter.
-// It will not work with the original file that produces the "pbrSpecularGlossiness" error.
+// Type definitions (can be very long, simplified here)
+type GLTFResult = GLTF & {
+  nodes: { [key: string]: THREE.Mesh };
+  materials: { [key: string]: THREE.MeshStandardMaterial };
+};
+
+const context = createContext(null);
+
+export function Instances({ children, ...props }) {
+  // IMPORTANT: Use the new renamed file here
+  const { nodes } = useGLTF('/energy_bar_v2.glb') as GLTFResult;
+  const instances = useMemo(() => {
+    const instances: { [key: string]: THREE.Mesh } = {};
+    for (const key in nodes) {
+        if (nodes[key].isMesh) {
+            // A simplified way to create the mapping for all meshes
+            instances[nodes[key].name] = nodes[key];
+        }
+    }
+    return instances;
+  }, [nodes]);
+
+  return (
+    <Merged meshes={instances} {...props}>
+      {(instances) => <context.Provider value={instances} children={children} />}
+    </Merged>
+  );
+}
 
 export const Energybar = forwardRef<THREE.Group, GroupProps>((props, ref) => {
-  // Make sure the path points to the NEW model in your /public folder
-  const { nodes, materials } = useGLTF('/granola_bar_3d.glb');
-  
+  const instances = useContext(context);
   return (
-    // By removing the internal scale, the scale prop passed from Scene.tsx will now work
     <group {...props} dispose={null} ref={ref}>
-      <mesh
-        // @ts-ignore
-        geometry={nodes.pCube3_aiStandardSurface1_0.geometry}
-        material={materials.aiStandardSurface1}
-      />
-      {/* Add other meshes from your model if there are any */}
+        {/* The gltfjsx output might be complex. This is a robust way to render it all */}
+        {Object.values(instances).map((instance: any) => (
+            <group key={instance.uuid} name={instance.name} position={instance.position} rotation={instance.rotation} scale={instance.scale}>
+                <mesh
+                    geometry={instance.geometry}
+                    material={instance.material}
+                />
+            </group>
+        ))}
     </group>
   );
 });
 
-useGLTF.preload('/granola_bar_3d.glb');
+// IMPORTANT: Use the new renamed file here as well
+useGLTF.preload('/energy_bar_v2.glb');
